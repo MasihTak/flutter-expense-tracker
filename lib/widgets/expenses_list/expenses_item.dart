d4c880171debefab59f2046eddd161175bd6e161
MasihTak <masihtak@outlook.com>
2024-01-29 17:19:24 +0100

    fix: dynamically display category icons

--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -25,7 +25,7 @@ class ExpenseItem extends StatelessWidget {
                 const Spacer(),
                 Row(
                   children: [
-                    const Icon(Icons.calendar_today),
+                    Icon(categoryIcons[expense.category]),
                     const SizedBox(width: 8),
                     Text(expense.date.toString()),
                   ],
