0ee3a3d60c7c0055653c4c8aeca6f6b5f7d6217d
MasihTak <masihtak@outlook.com>
2024-01-30 07:12:15 +0100

    chore: add comments

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
 
+// Creating a constant instance of Uuid
 const uuid = Uuid();
 
+/// A date formatter for formatting dates in year-month-day format.
 final formatter = DateFormat.yMd();
 
+/// An enumeration representing different categories of expenses.
 enum Category { food, travel, leisure, bills, work }
 
+/// A map linking each [Category] to a corresponding Material Design icon.
 const categoryIcons = {
   Category.food: Icons.fastfood,
   Category.travel: Icons.flight_takeoff,
@@ -16,7 +20,15 @@ const categoryIcons = {
   Category.work: Icons.work,
 };
 
+/// A class representing an expense.
+///
+/// Each expense has a unique identifier, a title, an amount, a date, and a category.
+/// The unique identifier is automatically generated upon creation of an expense.
 class Expense {
+  /// Creates a new expense.
+  ///
+  /// Requires a [title], an [amount], a [date], and a [category].
+  /// Automatically generates a unique identifier [id] for the expense.
   Expense({
     required this.title,
     required this.amount,
@@ -30,6 +42,9 @@ class Expense {
   final DateTime date;
   final Category category;
 
+  /// Returns the date of the expense in a formatted string.
+  ///
+  /// The date is formatted as year-month-day.
   String get formattedDate {
     return formatter.format(date);
   }
