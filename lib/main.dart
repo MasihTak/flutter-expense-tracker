77535012ffc7fb8fecc3529db4fc7053f235f8bb
MasihTak <masihtak@outlook.com>
2024-02-13 11:03:20 +0100

    style: update card theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme =
-    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 79, 181, 59));
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 79, 181, 59));
 
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-          colorScheme: kColorScheme,
-          appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer,
-          )),
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+      ),
       home: const Expenses(),
     ),
   );
